ba612a1837fbf15fe42a7f2566822e0cc98288c8
tony1c <antoniomario.cavuto@gmail.com>
2023-07-19 18:16:57 +0200

    kata nr. 5

---
+++ totalAmountOfPoints.js
@@ -0,0 +1,16 @@
+'use strict';
+/*
+Today's exercise
+Write a function calculateAmountOfPoints, that accepts one parameter: games, a valid array of strings, e.g. ["3:1", "2:2"].
+
+Points:
+
+x > y: 3 points
+x = y: 1 point
+x < y: 0 point
+The function should return a number. The number is the sum of the point(s) of every single game.
+
+Input: an array of strings.
+
+Output: a number.
+*/
