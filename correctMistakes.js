7b9edeadc4c1b628f02cfcfe872e373bb7155002
tony1c <antoniomario.cavuto@gmail.com>
2023-07-22 16:59:30 +0200

    katas

---
+++ correctMistakes.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/*
+Today's exercise
+Character recognition software is widely used to digitize printed texts. When documents are digitized character recognition softwares often make mistakes. Your task is to correct the errors in the digitized text. You only have to handle the following mistakes:
+
+S is misinterpreted as 5
+O is misinterpreted as 0
+I is misinterpreted as 1
+Write a function correctMistakes, that accepts one parameter: inputString, a valid string.
+
+The function should return a string. The string contains the inputString with the corrections.
+
+Input: a string.
+
+Output: a string with the corrections.
+*/
+
+// console.log(correctMistakes("0h!"));
+// "Oh!"
+
+// console.log(correctMistakes("1 l0ve m1ss1ss1pp1🧶!"));
+// "I lOve mIssIssIppI🧶!"
---
+++ countAmountOfEachCharacter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/*
+Today's exercise
+Write a function countAmountOfEachCharacter, that accepts one parameter: inputString, a valid string.
+
+The function should return an object. The object has a key for every character that exists at least once in the string.
+The value for each key is how many times that character exists in the string.
+
+Input: a string.
+
+Output: an object with keys for the existing characters and values for how many times that character exists in the string.
+*/
+
+// console.log(countAmountOfEachCharacter('bee'));
+// { b: 1, e: 2 }
