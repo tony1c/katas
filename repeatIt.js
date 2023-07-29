9631b4da7b12be30bcf5b58fd899349ea7ce7245
tony1c <antoniomario.cavuto@gmail.com>
2023-07-29 10:52:55 +0200

    katas

---
+++ repeatIt.js
@@ -0,0 +1,20 @@
+'use strict';
+/*
+Write a function repeatIt, that accepts two parameters: inputString and repetitions.
+
+Given a string, e.g. "Hi",
+and a number of repetitions, e.g. 2,
+return a string that repeats the input string n number of times, e.g. "HiHi":
+
+If the input is not a string, return "Not a string".
+
+Input: a string and a number.
+
+Output: a string.
+*/
+
+// console.log(repeatIt('Hi', 2));
+// "HiHi" ✅
+
+// console.log(repeatIt(999, 1));
+// "Not a string" ✅
