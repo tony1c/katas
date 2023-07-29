5199801701d0c0880c5b99268bc7bf523ae2e737
tony1c <antoniomario.cavuto@gmail.com>
2023-07-29 11:17:46 +0200

    katas

---
+++ amountOfLowercaseLetters.js
@@ -0,0 +1,17 @@
+'use strict';
+/*
+Write a function amountOfLowercaseLetters, that accepts one parameter: inputString.
+
+Given a string, e.g. "aB1c",
+return the number of lowercase letters in this string, e.g. 2:
+
+Input: a string.
+
+Output: a number.
+*/
+
+// console.log(amountOfLowercaseLetters('aB1c'));
+// 2 ✅
+
+// console.log(amountOfLowercaseLetters('123'));
+// 0 ✅
