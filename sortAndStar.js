b8135d00f7d56948ba575100ed7a37f43739aa16
tony1c <antoniomario.cavuto@gmail.com>
2023-07-29 21:37:58 +0200

    katas

---
+++ sortAndStar.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/*
+Today's exercise
+Source: Codewars
+
+Write a function sortAndStar, that accepts one parameter: stringArray.
+
+Given an array, e.g. ["We", "solve", "Katas"],
+sort it alphabetically (case-sensitive) and return the first string of the sorted array, with all characters separated by *** between each other, e.g. "K***a***t***a***s".
+
+Input: an array of strings.
+
+Output: a string.
+*/
+
+// console.log(sortAndStar(["We", "solve", "Katas"]));
+// "K***a***t***a***s" ✅
