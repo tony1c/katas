348053caddfb7062a9bc749d8dc934708e3ebf83
tony1c <antoniomario.cavuto@gmail.com>
2023-07-30 18:15:21 +0200

    kata

---
+++ everyPossibleSum.js
@@ -0,0 +1,26 @@
+'use strict';
+/*
+Today's exercise
+Today, we'll have a look at our first 7 kyu kata,
+meaning we slightly increase the difficulty.
+
+Source: Codewars
+
+Write a function everyPossibleSum, that accepts one parameter: myNumber.
+
+Given a number, e.g. 1234,
+return every possible sum of two digits, e.g. [ 3, 4, 5, 5, 6, 7 ].
+
+For this example, we calculate:
+[ 1 + 2, 1 + 3, 1 + 4, 2 + 3, 2 + 4, 3 + 4 ]
+
+Input: a number.
+
+Output: an array of numbers.
+*/
+
+// console.log(everyPossibleSum(1234));
+// [ 3, 4, 5, 5, 6, 7 ] ✅
+
+// console.log(everyPossibleSum(81596));
+// [ 9, 13, 17, 14, 6, 10, 7, 14, 11, 15 ] ✅
---
+++ positionInAlphabet.js
@@ -0,0 +1,21 @@
+'use strict';
+/*
+Today's exercise
+Source: Codewars
+
+Write a function positionInAlphabet, that accepts one parameter: myChar.
+
+Given a one-char string, e.g. "a",
+return the message "Position in Alphabet: [position]", e.g. "Position in Alphabet: 1".
+If the input is uppercase, handle it like a lowercase character.
+
+Input: a string.
+
+Output: a string.
+*/
+
+// console.log(positionInAlphabet("a"));
+// 1 ✅
+
+// console.log(positionInAlphabet("Z"));
+// 26  ✅
