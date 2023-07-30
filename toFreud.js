3c5156478fb77f007c870b4c9186d23ad0e1700f
tony1c <antoniomario.cavuto@gmail.com>
2023-07-30 17:02:35 +0200

    katas

---
+++ toFreud.js
@@ -0,0 +1,23 @@
+'use strict';
+/*
+Today's exercise
+Source: Codewars
+
+Today as small kata with a little bit of historical background about Sigmund Freud.
+
+Write a function toFreud, that accepts one parameter: myString.
+
+Given a string, e.g. "Sigmund Freud",
+return the string with every word replaced by the word "sex", e.g. "sex sex".
+An empty string or no argument should result in an empty string.
+
+Input: a string.
+
+Output: a string.
+*/
+
+// console.log(toFreud("Sigmund Freud"));
+// "sex sex" ✅
+
+// console.log(toFreud(""));
+// ""  ✅
