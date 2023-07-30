f85e2d2483b276060a3cfa0842dc9486e99316e6
tony1c <antoniomario.cavuto@gmail.com>
2023-07-30 18:53:49 +0200

    kata

---
+++ missingValues.js
@@ -0,0 +1,28 @@
+'use strict';
+/*
+Today's exercise
+Today, another 7 kyu kata,
+meaning we slightly increase the difficulty.
+
+Source: Codewars
+
+Write a function missingValues, that accepts one parameter: myArray.
+
+Given an array of number, e.g. [1, 1, 1, 2, 2, 3],
+
+find:
+
+the number x, that appears once, e.g. 3
+the number y, that appears twice, e.g. 2
+and return the product x * x * y, e.g. 18 (=> 3 x 3 x 2).
+
+Input: an array of numbers.
+
+Output: a number.
+*/
+
+// console.log([1, 1, 1, 2, 2, 3]);
+// 18 ✅
+
+// console.log([6, 5, 4, 100, 6, 5, 4, 100, 6, 5, 4, 200]);
+// 4000000 ✅
