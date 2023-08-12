a14bab506a3116ae1cf09a2821eddb1d1448be87
tony1c <antoniomario.cavuto@gmail.com>
2023-08-12 18:20:53 +0200

    katas

---
+++ toMinutesAndHours.js
@@ -0,0 +1,24 @@
+'use strict';
+/*
+Today's exercise
+Today, another 7 kyu kata,
+meaning we slightly increase the difficulty.
+
+Source: Codewars
+
+Write a function toMinutesAndHours, that accepts one parameter: seconds.
+
+Given a number, e.g. 3601,
+return a string describing how many hours and minutes comprise that many seconds, any remaining seconds left over are ignored,
+e.g. 1 hour(s) and 0 minute(s).
+
+Input: a number.
+
+Output: a string.
+*/
+
+// console.log(toMinutesAndHours(3600));
+// "1 hour(s) and 0 minute(s)" ✅
+
+// console.log(toMinutesAndHours(3601));
+// "1 hour(s) and 0 minute(s)" ✅
