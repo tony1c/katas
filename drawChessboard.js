aa1d669b7752777ae0e412206c217bd246bf9c6c
tony1c <antoniomario.cavuto@gmail.com>
2023-08-24 15:43:27 +0200

    katas

---
+++ drawChessboard.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*
+Write a function drawChessboard, that accepts two parameters: rows and columns.
+
+Given a rows number, e.g. 3, and a columns number, e.g. 3,
+return a chessboard as a two dimensional array, e.g.
+[
+  [ "O", "X", "O" ],
+  [ "X", "O", "X" ],
+  [ "O", "X", "O" ]
+]
+White should be represented by "O", black by "X", the first row starts with a "O".
+
+Input: two numbers.
+
+Output: an array of array(s).
+*/
+
+// console.log(drawChessboard(3, 3));
+/*
+[
+  [ "O", "X", "O" ],
+  [ "X", "O", "X" ],
+  [ "O", "X", "O" ]
+]
+*/
+// ✅
+
+// console.log(drawChessboard(2, 4));
+/*
+[
+  [ "O", "X", "O", "X" ],
+  [ "X", "O", "X", "O" ]
+]
+*/
+//  ✅
