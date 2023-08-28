91e3b959793fa30a1639d4d24f5ae9dd2b15662a
tony1c <antoniomario.cavuto@gmail.com>
2023-08-28 19:06:53 +0200

    katas

---
+++ higherVersion.js
@@ -0,0 +1,18 @@
+'use strict';
+/*
+Write a function higherVersion, that accepts two parameters: version1 and version2.
+
+Given two strings, e.g. "1.2.3" and "1.2.0", return if the first string is higher than the second, e.g. true.
+
+There are no leading zeros, e.g. 100.020.003 is given as 100.20.3.
+
+Input: two strings.
+
+Output: a boolean.
+*/
+
+// console.log(higherVersion('1.2.3', '1.2.0'));
+// true ✅
+
+// console.log(higherVersion('9', '10'));
+// false ✅
